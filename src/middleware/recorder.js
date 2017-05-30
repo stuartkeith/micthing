36069673322511d6f4c0fb057ff946f263f11fec
Stuart Keith <stuart@stuartkeith.com>
2017-05-30 19:22:40 +0100

    move recording logic out into the Recorder class

--- src/middleware/recorder.js
+++ src/middleware/recorder.js
@@ -5,26 +5,87 @@ const BUFFER_SIZE = 2048;
 const FADE_SAMPLES = 400;
 const MAX_RECORD_SECONDS = 4;
 
-function copyBuffer(buffer, length) {
-  const newBuffer = audioContext.createBuffer(buffer.numberOfChannels, length, buffer.sampleRate);
+function createBuffer(dataL, dataR, length) {
+  const buffer = audioContext.createBuffer(2, length, audioContext.sampleRate);
 
-  for (let i = 0; i < buffer.numberOfChannels; i++) {
-    const oldData = buffer.getChannelData(i);
-    const newData = newBuffer.getChannelData(i);
+  const bufferL = buffer.getChannelData(0);
+  const bufferR = buffer.getChannelData(1);
 
-    for (let j = 0; j < length; j++) {
-      newData[j] = oldData[j];
-    }
-
-    for (let j = 0; j < FADE_SAMPLES; j++) {
-      const volume = j / FADE_SAMPLES;
-
-      newData[j] *= volume;
-      newData[length - j - 1] *= volume;
-    }
+  for (let i = 0; i < length; i++) {
+    bufferL[i] = dataL[i];
+    bufferR[i] = dataR[i];
   }
 
-  return newBuffer;
+  for (let i = 0; i < FADE_SAMPLES; i++) {
+    const volume = i / FADE_SAMPLES;
+
+    bufferL[i] *= volume;
+    bufferR[i] *= volume;
+
+    bufferL[length - i - 1] *= volume;
+    bufferR[length - i - 1] *= volume;
+  }
+
+  return buffer;
+}
+
+class Recorder {
+  constructor(recordBufferLength) {
+    this.recordBufferL = new Float32Array(recordBufferLength);
+    this.recordBufferR = new Float32Array(recordBufferLength);
+
+    this.reset();
+    this.setInputBuffer([], []);
+  }
+
+  reset() {
+    this.isRecording = false;
+    this.consecutiveSamplesUnderThreshold = 0;
+    this.recordBufferIndex = 0;
+  }
+
+  setInputBuffer(inputBufferL, inputBufferR) {
+    this.inputBufferL = inputBufferL;
+    this.inputBufferR = inputBufferR;
+
+    this.inputBufferIndex = 0;
+  }
+
+  update(isEnabled, recordingThreshold, recordingThresholdSamples) {
+    while (this.inputBufferIndex < this.inputBufferL.length) {
+      if (this.recordBufferIndex === this.recordBufferL.length) {
+        return true;
+      }
+
+      if (this.isRecording && (this.consecutiveSamplesUnderThreshold >= recordingThresholdSamples)) {
+        return true;
+      }
+
+      const sampleL = this.inputBufferL[this.inputBufferIndex];
+      const sampleR = this.inputBufferR[this.inputBufferIndex];
+
+      const absSampleL = Math.abs(sampleL);
+      const absSampleR = Math.abs(sampleR);
+
+      if (isEnabled && ((absSampleL >= recordingThreshold) || (absSampleR >= recordingThreshold))) {
+        this.consecutiveSamplesUnderThreshold = 0;
+        this.isRecording = true;
+      } else {
+        this.consecutiveSamplesUnderThreshold++;
+      }
+
+      this.inputBufferIndex++;
+
+      if (this.isRecording) {
+        this.recordBufferL[this.recordBufferIndex] = sampleL;
+        this.recordBufferR[this.recordBufferIndex] = sampleR;
+
+        this.recordBufferIndex++;
+      }
+    }
+
+    return false;
+  }
 }
 
 export default function recorder(store) {
@@ -41,70 +102,36 @@ export default function recorder(store) {
       const audioInput = audioContext.createMediaStreamSource(action.mediaStream);
       const audioRecorder = audioContext.createScriptProcessor(BUFFER_SIZE, 2, 2);
       const recordBufferLength = Math.floor(MAX_RECORD_SECONDS * audioContext.sampleRate);
-      const recordBuffer = audioContext.createBuffer(2, recordBufferLength, audioContext.sampleRate);
-
-      const recordDataL = recordBuffer.getChannelData(0);
-      const recordDataR = recordBuffer.getChannelData(1);
-
-      let isRecording = false;
-      let bufferIndex = 0;
-      let samplesUnderThreshold = 0;
+      const recorder = new Recorder(recordBufferLength);
 
       audioRecorder.onaudioprocess = function (event) {
-        const state = store.getState();
+        let state = store.getState();
 
-        if (!state.isRecording) {
-          if (isRecording) {
-            isRecording = false;
+        const inputBufferL = event.inputBuffer.getChannelData(0);
+        const inputBufferR = event.inputBuffer.getChannelData(1);
 
-            store.dispatch(capturingStop());
+        recorder.setInputBuffer(inputBufferL, inputBufferR);
+
+        while (recorder.update(state.isRecording, state.recordingThreshold, state.recordingThresholdSamples)) {
+          const buffer = createBuffer(recorder.recordBufferL, recorder.recordBufferR, recorder.recordBufferIndex);
+
+          recorder.reset();
+
+          if (state.layers.length < 5) {
+            store.dispatch(layerAdd(state.nextLayerId, buffer));
+
+            if (state.layers.length === 0) {
+              store.dispatch(playbackStart());
+            }
           }
 
-          return;
+          state = store.getState();
         }
 
-        const inputDataL = event.inputBuffer.getChannelData(0);
-        const inputDataR = event.inputBuffer.getChannelData(1);
-
-        for (let i = 0; i < inputDataL.length; i++) {
-          const sampleMeetsThreshold = (inputDataL[i] >= state.recordingThreshold) || (inputDataR[i] >= state.recordingThreshold);
-          const hasExpired = samplesUnderThreshold >= state.recordingThresholdSamples;
-          const noRemainingSpace = bufferIndex >= recordBufferLength;
-
-          if (!isRecording && sampleMeetsThreshold) {
-            isRecording = true;
-            bufferIndex = 0;
-            samplesUnderThreshold = 0;
-
-            store.dispatch(capturingStart());
-          } else if (isRecording && (hasExpired || noRemainingSpace)) {
-            isRecording = false;
-
-            const buffer = copyBuffer(recordBuffer, Math.min(bufferIndex, recordBufferLength - 1));
-
-            store.dispatch(capturingStop());
-
-            if (state.layers.length < 5) {
-              store.dispatch(layerAdd(state.nextLayerId, buffer));
-
-              if (state.layers.length === 0) {
-                store.dispatch(playbackStart());
-              }
-            }
-          }
-
-          if (isRecording) {
-            recordDataL[bufferIndex] = inputDataL[i];
-            recordDataR[bufferIndex] = inputDataR[i];
-
-            bufferIndex++;
-
-            if (sampleMeetsThreshold) {
-              samplesUnderThreshold = 0;
-            } else {
-              samplesUnderThreshold++;
-            }
-          }
+        if (!state.isCapturing && recorder.isRecording) {
+          store.dispatch(capturingStart());
+        } else if (state.isCapturing && !recorder.isRecording) {
+          store.dispatch(capturingStop());
         }
       };
 
