88aa6991ce9e897f49d19250a8b28060ccad80d9
Stuart Keith <stuart@stuartkeith.com>
2017-06-01 20:04:02 +0100

    use copyFrom/ToChannel instead of getChannelData to avoid memory allocation

--- src/middleware/recorder.js
+++ src/middleware/recorder.js
@@ -8,25 +8,21 @@ const FADE_SAMPLES = 400;
 const MAX_RECORD_SECONDS = 4;
 
 function createBuffer(dataL, dataR, length) {
+  // note that this mutates the dataL and dataR arrays.
   const buffer = audioContext.createBuffer(2, length, audioContext.sampleRate);
-
-  const bufferL = buffer.getChannelData(0);
-  const bufferR = buffer.getChannelData(1);
-
-  for (let i = 0; i < length; i++) {
-    bufferL[i] = dataL[i];
-    bufferR[i] = dataR[i];
-  }
 
   for (let i = 0; i < FADE_SAMPLES; i++) {
     const volume = i / FADE_SAMPLES;
 
-    bufferL[i] *= volume;
-    bufferR[i] *= volume;
+    dataL[i] *= volume;
+    dataR[i] *= volume;
 
-    bufferL[length - i - 1] *= volume;
-    bufferR[length - i - 1] *= volume;
+    dataL[length - i - 1] *= volume;
+    dataR[length - i - 1] *= volume;
   }
+
+  buffer.copyToChannel(dataL, 0);
+  buffer.copyToChannel(dataR, 1);
 
   return buffer;
 }
@@ -96,6 +92,8 @@ class Recorder {
 
 export default function recorder(store) {
   const audioRecorder = audioContext.createScriptProcessor(BUFFER_SIZE, 2, 2);
+  const inputBufferL = new Float32Array(BUFFER_SIZE);
+  const inputBufferR = new Float32Array(BUFFER_SIZE);
   const maxSampleMessageBus = new MessageBus();
   const recordBufferLength = Math.floor(MAX_RECORD_SECONDS * audioContext.sampleRate);
   const recorder = new Recorder(recordBufferLength);
@@ -127,8 +125,8 @@ export default function recorder(store) {
   audioRecorder.onaudioprocess = function (event) {
     let state = store.getState();
 
-    const inputBufferL = event.inputBuffer.getChannelData(0);
-    const inputBufferR = event.inputBuffer.getChannelData(1);
+    event.inputBuffer.copyFromChannel(inputBufferL, 0);
+    event.inputBuffer.copyFromChannel(inputBufferR, 1);
 
     recorder.setInputBuffer(inputBufferL, inputBufferR);
 
