0aaff49a5fad87fedce9ad3fa9dd2c322009b82e
Stuart Keith <stuart@stuartkeith.com>
2017-06-01 19:45:40 +0100

    add recording threshold indicator to VolumeMeter

--- src/components/VolumeMeter.js
+++ src/components/VolumeMeter.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { recordingListenerAdd, recordingListenerRemove } from '../actions';
+import { recordingListenerAdd, recordingListenerRemove, recordingThresholdSet } from '../actions';
 
 class VolumeMeter extends React.Component {
   constructor(props) {
     super(props);
 
     this.audioInputListener = this.audioInputListener.bind(this);
+    this.onClick = this.onClick.bind(this);
     this.setElement = this.setElement.bind(this);
   }
 
@@ -18,12 +19,23 @@ class VolumeMeter extends React.Component {
     this.props.recordingListenerRemove(this.audioInputListener);
   }
 
-  shouldComponentUpdate() {
-    return false;
-  }
-
   audioInputListener(maxSample) {
     this.element.style.transform = `scale3d(${maxSample}, 1, 1)`;
+  }
+
+  onClick(event) {
+    let element = event.currentTarget;
+    let x = event.pageX;
+
+    while (element) {
+      x -= element.offsetLeft;
+
+      element = element.offsetParent;
+    }
+
+    const value = x / event.currentTarget.offsetWidth;
+
+    this.props.recordingThresholdSet(value);
   }
 
   setElement(element) {
@@ -31,16 +43,29 @@ class VolumeMeter extends React.Component {
   }
 
   render() {
+    const { recordingThreshold } = this.props;
+
+    const arrowStyle = {
+      transform: `translate3d(${recordingThreshold * 100}%, 0, 0)`
+    };
+
     return (
-      <div className="w4 h1 relative">
-        <div className="absolute absolute--fill bg-black" />
+      <div className="h2 bg-black overflow-hidden relative" onClick={this.onClick}>
         <div ref={this.setElement} className="absolute absolute--fill bg-white transform-origin-left" />
+        <div className="absolute absolute--fill transform-origin-left bl bw1 b--gold" style={arrowStyle} />
       </div>
     );
   }
 }
 
-export default connect(null, {
+function mapStateToProps(state) {
+  return {
+    recordingThreshold: state.recordingThreshold
+  };
+}
+
+export default connect(mapStateToProps, {
   recordingListenerAdd,
-  recordingListenerRemove
+  recordingListenerRemove,
+  recordingThresholdSet
 })(VolumeMeter);
