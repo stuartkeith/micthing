1db90fac851772c0b5f25fa10e06b7433021e1e4
Stuart Keith <stuart@stuartkeith.com>
2017-06-11 14:07:19 +0100

    add default startInChannel argument value for copyFromChannel/copyToChannel polyfills

--- src/webaudio/polyfills.js
+++ src/webaudio/polyfills.js
@@ -1,6 +1,6 @@
 // adapted from https://github.com/mohayonao/web-audio-api-shim/blob/master/src/AudioBuffer.js
 if (typeof AudioBuffer !== 'undefined' && !AudioBuffer.prototype.hasOwnProperty('copyFromChannel')) {
-  AudioBuffer.prototype.copyFromChannel = function (destination, channelNumber, startInChannel) {
+  AudioBuffer.prototype.copyFromChannel = function (destination, channelNumber, startInChannel = 0) {
     const source = this.getChannelData(channelNumber).subarray(startInChannel);
 
     destination.set(source.subarray(0, Math.min(source.length, destination.length)));
@@ -9,7 +9,7 @@ if (typeof AudioBuffer !== 'undefined' && !AudioBuffer.prototype.hasOwnProperty(
 
 // adapted from https://github.com/mohayonao/web-audio-api-shim/blob/master/src/AudioBuffer.js
 if (typeof AudioBuffer !== 'undefined' && !AudioBuffer.prototype.hasOwnProperty('copyToChannel')) {
-  AudioBuffer.prototype.copyToChannel = function (source, channelNumber, startInChannel) {
+  AudioBuffer.prototype.copyToChannel = function (source, channelNumber, startInChannel = 0) {
     const clipped = source.subarray(0, Math.min(source.length, this.length - (startInChannel)));
 
     this.getChannelData(channelNumber).set(clipped, startInChannel);
