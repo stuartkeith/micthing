602e74ed0145fb9dfff80e76da60de0995eca88c
Stuart Keith <stuart@stuartkeith.com>
2019-03-25 12:12:27 +1100

    Button - add flex-none

--- src/components/Button.js
+++ src/components/Button.js
@@ -6,7 +6,7 @@ function Button({ children, disabled, isDown, onClick }) {
     <button
       disabled={disabled}
       className={cn(
-        'button-reset input-reset db pa0 bn border-box lh-solid w4 h2 outline-0 pointer',
+        'button-reset flex-none input-reset db pa0 bn border-box lh-solid w4 h2 outline-0 pointer',
         disabled ? 'o-50' : null,
         isDown ? 'bg-dark-gray light-gray b--near-black button-border-top' : null,
         !isDown ? 'bg-gold dark-gray b--orange button-border-bottom' : null
